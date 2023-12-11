3427f035073dbbc1619f1ee34e5c349e82947e1e
Ritabrata Pal <rito3668@gmail.com>
2023-12-11 11:51:11 +0530

    hooks context routes pages setup

---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,8 @@
+import './Dashboard.css'
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div>Dashboard</div>
+  )
+}
---
+++ src/pages/project/Project.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Project.css'
+export default function Project() {
+  return (
+    <div>Project</div>
+  )
+}
