3bd6ee2b0f1b0ac52ed5dca2b99426031b3fa101
Ritabrata Pal <rito3668@gmail.com>
2023-12-11 12:58:36 +0530

    signup and useSignup

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react'
 import { projectAuth } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
-
+import { projectStorage } from '../firebase/config'
 export const useSignup = () => {
   const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const { dispatch } = useAuthContext()
 
-  const signup = async (email, password, displayName) => {
+  const signup = async (email, password, displayName, thumbnail) => {
     setError(null)
     setIsPending(true)
   
@@ -20,8 +20,13 @@ export const useSignup = () => {
         throw new Error('Could not complete signup')
       }
 
-      // add display name to user
-      await res.user.updateProfile({ displayName })
+      // upload user thumbnail
+      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`
+      const img = await projectStorage.ref(uploadPath).put(thumbnail)
+      const photoURL = await img.ref.getDownloadURL()
+
+      // add display AND PHOTO_URL name to user
+      await res.user.updateProfile({ displayName, photoURL})
 
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user })
