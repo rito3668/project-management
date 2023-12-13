d5fba5873622d12fcb2750934cf8f0b97da9713e
Ritabrata Pal <rito3668@gmail.com>
2023-12-13 12:10:26 +0530

    making the details page

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useDocument } from '../../hooks/useDocument'
 import './Project.css'
+import {useParams} from 'react-router-dom'
 export default function Project() {
+  const {id} = useParams()
+  const {error,document} = useDocument("projects",id)
+  if(error){
+    return <div className='error'>{error}</div>
+  }
+  if(!document){
+    return <div className='loading'>loading...</div>
+  }
   return (
-    <div>Project</div>
+    <div className='project-details'>
+      <h1>{projectAuth.name}</h1>
+    </div>
   )
 }
